d4042d64168b4174d5c8b71d954d38373dcd370f
Maksim Kotov <4253046+aksioto@users.noreply.github.com>
2025-12-24 02:12:17 +0300

    Step 2

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -11,7 +11,15 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "gendiff",
 		Usage: "Compares two configuration files and shows a difference.",
-		Flags: []cli.Flag{},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "format",
+				Aliases:     []string{"f"},
+				Value:       "stylish",
+				DefaultText: "\"stylish\"",
+				Usage:       "output format",
+			},
+		},
 		Action: func(ctx context.Context, c *cli.Command) error {
 			return nil
 		},
